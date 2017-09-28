8fb3c5de0d39eca82260448b1033105540f2776b
NGUYEN DUC VIET <vietnd@foobla.com>
2017-09-28 08:52:13 +0700

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 gulp.task('scss', function () {
-    gulp.src('assets/sass/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('assets/css/'))
+    gulp.src('assets/sass/style.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest(''))
 });
 gulp.task('watch', function () {
-    gulp.watch(['assets/sass/*.scss'], ['scss']);
+    gulp.watch(['assets/sass/style.scss'], ['scss']);
 });
 gulp.task('default', ['scss', 'watch']);
