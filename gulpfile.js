13e9b4cc06979e9a86961664ab1dddd6fb950294
NGUYEN DUC VIET <vietnd@foobla.com>
2017-10-04 11:08:29 +0700

    add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-gulp.task('scss', function () {
-    gulp.src('assets/sass/style.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest(''))
-});
+
 gulp.task('watch', function () {
     gulp.watch(['assets/sass/**/*.scss'], ['scss']);
+});
+
+gulp.task('scss', function () {
+    gulp.src('assets/sass/style.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest(''))
 });
 gulp.task('default', ['scss', 'watch']);
 //hehe
