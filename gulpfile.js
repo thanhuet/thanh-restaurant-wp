f1213c6dc9185e2c4a36a5a9dcc3c3e6fb3d1efd
NGUYEN DUC VIET <vietnd@foobla.com>
2017-10-04 11:04:56 +0700

    add cmt gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ gulp.task('scss', function () {
     gulp.src('assets/sass/style.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest(''))
 });
 gulp.task('watch', function () {
-    gulp.watch(['assets/sass/style.scss'], ['scss']);
+    gulp.watch(['assets/sass/**/*.scss'], ['scss']);
 });
 gulp.task('default', ['scss', 'watch']);
+//hehe
