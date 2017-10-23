ed43731ef5d1cd078af5db0248680a38716efb31
thanhuet <thanh9659@gmail.com>
2017-10-23 15:04:47 +0700

    fix title position in blog detail

--- templates/template-parts/content-single.php
+++ templates/template-parts/content-single.php
@@ -10,23 +10,24 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="content-inner">
+        <header class="entry-header">
+            <?php
+            if ( is_single() ) {
+                the_title( '<h1 class="entry-title">', '</h1>' );
+            } else {
+                the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            }
+            ?>
+        </header><!-- .entry-header -->
+
+        <?php thim_entry_meta(); ?>
+
 		<div class="entry-top">
 			<?php if ( get_theme_mod( 'blog_single_feature_image', true ) ) :
 				do_action( 'thim_entry_top', 'full' );
             endif; ?>
 		</div><!-- .entry-top -->
 		<div class="entry-content">
-			<header class="entry-header">
-				<?php
-				if ( is_single() ) {
-					the_title( '<h1 class="entry-title">', '</h1>' );
-				} else {
-					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-				}
-				?>
-			</header><!-- .entry-header -->
-
-			<?php thim_entry_meta(); ?>
 
 			<div class="entry-description">
 				<?php
