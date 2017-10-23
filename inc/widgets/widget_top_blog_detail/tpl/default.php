9ada05cde93a5cf37aff75ee76bb543645123073
thanhuet <thanh9659@gmail.com>
2017-10-23 14:52:19 +0700

    style widget top blog detail ( image + title image + after title image)

--- inc/widgets/widget_top_blog_detail/tpl/default.php
+++ inc/widgets/widget_top_blog_detail/tpl/default.php
@@ -2,10 +2,10 @@
     $attachment = wp_get_attachment_image_src($instance['image']);
     $url_image = $attachment['0'];
 ?>
-<div>
-    <div><img src="<?php echo $url_image; ?>"></div>
+<div class="widget-top-blog-detail">
+    <div class="widget-logo-image-blog-detail"><img src="<?php echo $url_image; ?>"></div>
 
-    <div><p><?php echo esc_attr( $instance['title'] ) ?></p></div>
+    <div class="widget-title-blog-detail"><p><?php echo esc_attr( $instance['title'] ) ?></p></div>
 
-    <div><?php echo esc_attr( $instance['description'] ) ?></div>
+    <div class="widget-description-blog-detail"><?php echo esc_attr( $instance['description'] ) ?></div>
 </div>
