e60238019e169048e280575510f0a2e25fc888f9
longvv <longvv@foobla.com>
2017-12-08 10:13:50 +0700

    UPDATE: demo data

--- inc/data/demos.php
+++ inc/data/demos.php
@@ -3,7 +3,7 @@ return array(
 	'demo-01' => array(
 		'title'            => esc_html__( 'Demo 01', 'restaurant-wp' ),
 		'demo_url'         => 'http://restaurantwp.thimpress.com',
-		'thumbnail_url'    => 'https://raw.githubusercontent.com/ThimPressWP/demo-data/master/restaurantwp/demos/assets/demo-1/screenshot.png',
+		'thumbnail_url'    => 'https://raw.githubusercontent.com/ThimPressWP/demo-data/master/restaurantwp/demos/assets/demo-01/screenshot.png',
 		'plugins_required' => array(
 			'contact-form-7',
 			'siteorigin-panels',
