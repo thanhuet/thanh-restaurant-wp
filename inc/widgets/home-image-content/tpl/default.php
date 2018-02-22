01d743dd1fc771e59907bcd334d5d960ecbe7d56
thanhuet <thanh9659@gmail.com>
2018-02-22 17:24:13 +0700

    fix home again

--- inc/widgets/home-image-content/tpl/default.php
+++ inc/widgets/home-image-content/tpl/default.php
@@ -1,9 +1,11 @@
 <?php
-$attachment = wp_get_attachment_image_src( $instance['image'], 'full' );
-$image_url  = $attachment[0];
-$size_image = getimagesize( $image_url );
-$imageCrop  = $size_image[0] > 450 && $size_image[1]>559 ? thim_aq_resize( $image_url, 450, 559, 1 ) : $image_url;
+$attachment = wp_get_attachment_image_src($instance['image'], 'full');
+$image_url = $attachment[0];
+$size_image = getimagesize($image_url);
+if ($image_url != null):
+    $imageCrop = thim_aq_resize($image_url, 800, 500, 1);
+endif;
 ?>
 <div class="home-image-content">
-    <img src="<?php echo esc_url( $imageCrop ); ?>">
+    <img src="<?php echo esc_url($imageCrop); ?>">
 </div>
