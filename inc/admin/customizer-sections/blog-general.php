9df1543ed7e78273ebfbd980374130a46d06774d
thanhuet <thanh9659@gmail.com>
2018-06-18 12:00:59 +0700

    test test_new_branch

--- inc/admin/customizer-sections/blog-general.php
+++ inc/admin/customizer-sections/blog-general.php
@@ -128,7 +128,7 @@ thim_customizer()->add_group( array(
                         ),
                     ),
                 ),
-                // Select Post Column Numbers For Related Post dfd
+                // Select Post Column Numbers For Related Post dfd sfdsadf
                 array(
                     'type'            => 'slider',
                     'id'              => 'blog_single_related_post_column',
